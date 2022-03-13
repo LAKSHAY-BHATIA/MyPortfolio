ce8fed560cdd2dcc0f0a2695084c3601b5940ff5
lakshaybhatia <lakshaybhatia90@yahoo.com>
2022-03-13 13:57:54 +0530

    Resume updated

--- src/components/data/Work.js
+++ src/components/data/Work.js
@@ -2,7 +2,7 @@ export const WorkData = [
     {
         company:"TCS, Gurugram India",
         designation:"System Engineer",
-        dateJoining:"06-05-2021",
+        dateJoining:"06/05/2021",
         dateEnd:"Present",
         companyLogo:"https://mma.prnewswire.com/media/1477373/TCS_Logo.jpg",
         work:"Currently Working as a Backend Developer. Worked on a project where I have created web API services based on .NET Core Framework and MS Azure."
@@ -19,8 +19,8 @@ export const WorkData = [
     {
         company:"Imanent Solution, Mohali,India",
         designation:"Software Engineer Intern",
-        dateJoining:"21-09-2020",
-        dateEnd:"30-11/2020",
+        dateJoining:"21/09/2020",
+        dateEnd:"30/11/2020",
         companyLogo:"https://media-exp1.licdn.com/dms/image/C510BAQHshVIDD1lKcg/company-logo_200_200/0/1532345012125?e=2159024400&v=beta&t=mPljdht3AJBAOOWrDl4DtW_YcFUfLl8lOo24L4-n0Vg",
         work:"Worked as a Full stack web developer. Worked on several technologies & on different projects that involves - React, React Native, NodeJS etc"
     },
